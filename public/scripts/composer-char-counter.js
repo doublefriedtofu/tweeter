1bb448abd916fb08e2e9f3fbee89f1c19c320559
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-08 21:10:05 -0500

    initial commit; character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log("ready!")
+
+  // for ID
+  $('#tweet-text').on("keyup keydown", function() {
+    const maxLength = 140 - $(this).val().length
+    if (maxLength < 0) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "#545149");
+    }
+    $('.counter').text(maxLength);
+  });
+});
