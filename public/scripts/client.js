d1d36d8c1827eaeb5b31d1b63a2d64d3d8b5e6fc
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-09 22:35:22 -0500

    added form validation function; will not submit unless requirements are met

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,37 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 $(document).ready(function() {
+  
   const renderTweets = function(tweets) {
     // loops through tweets
     for (let tweet of tweets) {
-      console.log('tweet:', tweet);
       // calls createTweetElement for each tweet
       const tweetElement = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
@@ -62,19 +37,53 @@ $(document).ready(function() {
         
         <footer>
           <div class="date">
-            <a>${tweet.created_at}</a>
+            <a>${timeago.format(tweet.created_at)}</a>
           </div>
           <div>
-            <button class="fa-solid  fa-flag"></button>
-            <button class="fa-solid fa-retweet"></button>
-            <button class="fa-solid fa-heart"></button>
+          <button class="fa-solid  fa-flag"></button>
+          <button class="fa-solid fa-retweet"></button>
+          <button class="fa-solid fa-heart"></button>
           </div>
-        
-        </footer>
-        </div>`;
+          
+          </footer>
+          </div>`;
 
     return $tweet;
   };
 
-  renderTweets(data);
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    const formData = $(this).serialize();
+
+    const textAreaInput = $('textarea').val()
+    const textAreaLength = (textAreaInput.length)
+    if(!textAreaLength) {
+      return alert("Your Tweet is empty!") 
+    }
+    if (textAreaLength > 140) {
+      return alert("Your Tweet is too long!")
+    }
+
+    let url = '/tweets';
+
+    $.ajax({
+      url: url,
+      method: "POST",
+      data: formData,
+    }).then(() => {
+      loadTweets();
+    });
+
+    this.reset();
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then((tweetsData) => {
+        renderTweets(tweetsData);
+      });
+  };
+
+
+  loadTweets();
 });
